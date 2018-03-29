b8688fb4ea5941757dc0305c359d92c2fb8d044a
arye-dov-eidelman <aryedov.e@gmail.com>
2018-03-29 16:28:37 -0400

    designed the top level cli

--- lib/google_experiments/CLI.rb
+++ lib/google_experiments/CLI.rb
@@ -5,7 +5,23 @@ module GoogleExperiments
     end
     
     def run
-      puts 'CLI running'
+      puts "\nBrowse cool experiments from google."
+      puts "\nPlease choose a catigory and enter it's number"
+      puts "\n1. Voice"
+      puts "     Explore voice interaction in fun new ways."
+      puts "\n2. Arts & Culture"
+      puts "     See what happens at the crossroads of art and technology."
+      puts "\n3. AR"
+      puts "     Explorations in augmented reality."
+      puts "\n4. WebVR"
+      puts "     See what’s possible with virtual reality on the web."
+      puts "\n5. AI"
+      puts "     See what happens when anyone can play with machine learning."
+      puts "\n6. Android"
+      puts "     See how creativity and code come together on Android."
+      puts "\n7. Chrome"
+      puts "     See how creativity and code come together on the web."
+      puts ""
     end
   end
 end
