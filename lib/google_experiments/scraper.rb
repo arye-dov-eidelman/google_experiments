af9e466665aac51e9404ee9f3026e5e3b8140782
arye-dov-eidelman <aryedov.e@gmail.com>
2018-04-09 21:35:00 -0400

    [ 1.5 ] scraper.homepage - scrapes and returns a homepage hash

--- lib/google_experiments/scraper.rb
+++ lib/google_experiments/scraper.rb
@@ -7,18 +7,18 @@ module GoogleExperiments
     def homepage
       doc = open("https://experiments.withgoogle.com")
       doc = Nokogiri::HTML(doc)
-      # binding.pry
       about = doc.css("#main .wrapper .with60")[0].text
-      categories = [
-      #   {link: "/voice", title: "Voice", subtitle: "Explore voice interaction in fun new ways."},
-      #   {link: "/arts-culture", title: "Arts & Culture", subtitle: "See what happens at the crossroads of art and technology."},
-      #   {link: "/ar", title: "AR", subtitle: "Explorations in augmented reality."},
-      #   {link: "/webvr", title: "WebVR", subtitle: "See what’s possible with virtual reality on the web."},
-      #   {link: "/ai", title: "AI", subtitle: "See what happens when anyone can play with machine learning."},
-      #   {link: "/android", title: "Android", subtitle: "See how creativity and code come together on Android."},
-      #   {link: "/chrome", title: "Chrome", subtitle: "See how creativity and code come together on the web."}
-      ]
-      
+      categories = []
+      doc.css(".box-wrap").each do |card|
+        break if !card.css(".headline")[0]
+        category = {}
+        card.css('br').each{|br| br.replace(" ")}
+        category[:title] = card.css(".headline")[0].text
+        category[:subtitle] = card.css(".subline")[0].text
+        category[:link] = card.css("a")[0].attribute('href').to_s
+        categories << category
+      end
+
       {categories: categories, about: about}
     end
   end
