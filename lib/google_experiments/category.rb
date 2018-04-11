ad9169e9e62e02c54793599fa0806517fee701d1
arye-dov-eidelman <aryedov.e@gmail.com>
2018-04-11 15:48:18 -0400

    fixed @main_page being nil

--- lib/google_experiments/category.rb
+++ lib/google_experiments/category.rb
@@ -3,13 +3,13 @@ module GoogleExperiments
     attr_accessor :title, :subtitle, :link, :experiments, :about, :main_list
     def initialize(data)
       data.each{|k, v| self.send("#{k}=", v)}
+      @experiments = []
       @main_list = @experiments
     end
 
     def scrape_page
       data = SCRAPER.category(@link)
       @about = data[:about]
-      @experiments = []
       data[:experiments].each do |experiment_data|
         @experiments << Experiment.new(experiment_data)
       end
--- lib/google_experiments/homepage.rb
+++ lib/google_experiments/homepage.rb
@@ -2,6 +2,7 @@ module GoogleExperiments
   class Homepage < Page
     attr_reader :categories, :about, :main_list
     def initialize
+      @categories = []
       @main_list = @categories
       self.get_page
     end
@@ -9,7 +10,6 @@ module GoogleExperiments
     def scrape_page
       data = SCRAPER.homepage
       @about = data[:about]
-      @categories = []
       data[:categories].each do |category_data|
         @categories << Category.new(category_data)
       end
