3151fbd33548d9f780b0e1f80b4ca557a61cf323
arye-dov-eidelman <aryedov.e@gmail.com>
2018-04-12 18:00:06 -0400

    shortend and removed some code and added more comments to the cli

--- lib/google_experiments/CLI.rb
+++ lib/google_experiments/CLI.rb
@@ -13,7 +13,7 @@ module GoogleExperiments
     end
 
     def get_and_validate_input(max)
-      # gets user input and checks for keywords and valid numbers in the
+      # gets user input and checks for commands and valid numbers in the
       # provided range. Not to be relied on for it's return value instead
       # use @user_input after running get_and_validate_input(max)
 
@@ -48,57 +48,74 @@ module GoogleExperiments
     end
 
     def welcome_page
+      # display the homepage options from experiments.withgoogle.com
+
       @homepage ||= Homepage.new
       puts "\nBrowse Cool Experiments From Google."
       puts "\n#{@homepage.about}"
-      # puts ignores the last \n (if i didnt want it why would i put it there?) hence im using print 
+
+      # puts ignores the last \n hence im using print
+      # if i didnt want it why would i put it there?
       print "\nPlease choose a catigory and enter it's number\n\n"
 
+      # prints a list of categories with an option number title and subtitle
+      # for each category properly spaced in 2 columns
       @homepage.categories.each.with_index do |catigory, i|
         puts "#{i+1}. #{catigory.title}#{@homepage.tab(i)}#{catigory.subtitle}"
       end
-
       puts ""
+
+      # gets user input and navigates to the direct page
       self.get_and_validate_input(@homepage.categories.length)
       self.catigory_page(@homepage.categories[@user_input])
     end
 
     def catigory_page(catigory)
+      # display all experiments using a specifec technology or topic
+
+      # prints title subtitle and more about this catigory
       puts "\n\n#{catigory.title} - #{catigory.subtitle}"
       catigory.get_page
       puts "\n#{catigory.about}"
       print "\nPlease choose an experiment and enter it's number\n\n"
-
+      
+      # prints a list of experiments with an option number title and author
+      # for each experiment properly spaced in 2 columns
       catigory.experiments.each.with_index do |experiment, i|
         puts "#{i+1}. #{experiment.title}#{catigory.tab(i)}#{experiment.author}"
       end
-
       puts ""
+
+      # gets user input and navigates to the direct page
       self.get_and_validate_input(catigory.experiments.length)
       self.experiment_page(catigory.experiments[@user_input])
     end
 
     def experiment_page(experiment)
+      # tell the user all about this experiment
+      # with options to try it out or veiw the soure code if possible 
+
+      # print title author intro and moore about this experiment
       puts "\n\n#{experiment.title} - #{experiment.author}"
       experiment.get_page
       puts "\n#{experiment.intro}"
       puts "\n#{experiment.about}"
       puts ""
-      option_number = 0
 
-      puts "0. home"
+      # if possible display options to try the experiment or veiw the soure code 
+      puts "#{option_number = 0}. home"
       if experiment.launch
-        option_number += 1
-        puts "#{option_number}. Launch in browser"
+        puts "#{option_number += 1}. Launch in browser"
       end
       if experiment.code
-        option_number += 1
-        puts "#{option_number}. view on github"
+        puts "#{option_number += 1}. view on github"
       end
-
       puts ""
+
+      # gets user input
       self.get_and_validate_input(option_number)
       
+      # and navigates to the direct page in the brower or go back to thew welcome page
       if @user_input == 0 && experiment.launch
         puts "launching in browser..."
         Launchy.open(experiment.launch)
@@ -106,9 +123,6 @@ module GoogleExperiments
         puts "opening on github..."
         Launchy.open(experiment.code)
       end
-
-      puts "\n0. home"
-      self.get_and_validate_input(1)
     end
   end
 end
